5b8bb04d25ec3a826fe1fa563e3445deb6cad76b
keun66080823-ui <keun66080823@gmail.com>
2025-10-15 17:41:08 +0900

    성능 향상 보조스트림+객체 입출력 보조스트림+문자 기반 스트림+직렬화와 역직렬화

---
+++ 12_io/src/edu/kh/io/pack4/run/Character.java
@@ -0,0 +1,15 @@
+package edu.kh.io.pack4.run;
+
+import edu.kh.io.pack4.model.service.CharacterService;
+
+public class Character {
+
+	public static void main(String[] args) {
+		CharacterService service = new CharacterService();
+		
+		//service.fileInput();
+		service.fileOutput();
+		
+	}
+
+}
