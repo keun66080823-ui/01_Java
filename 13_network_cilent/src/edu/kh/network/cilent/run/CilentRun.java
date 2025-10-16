dd93e326e924d80fc836b4a9e23aac3f8ca7f6ea
keun66080823-ui <keun66080823@gmail.com>
2025-10-16 12:38:26 +0900

    네트워크 실습내용(클라이언트)

---
+++ 13_network_cilent/src/edu/kh/network/cilent/run/CilentRun.java
@@ -0,0 +1,12 @@
+package edu.kh.network.cilent.run;
+
+import edu.kh.network.cilent.model.service.ClientService;
+
+public class CilentRun {
+
+	public static void main(String[] args) {
+		new ClientService().clientStart();
+
+	}
+
+}
